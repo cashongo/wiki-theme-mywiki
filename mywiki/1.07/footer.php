4de365d5beed630a5d67873322f77aa17c4f7c36
themedropbox <themedropbox@8c925393-aaf8-4f5c-984e-1f1965effe94>
2014-05-22 06:55:44 +0000

    New version of MyWiki - 1.07

---
+++ mywiki/1.07/footer.php
@@ -0,0 +1,46 @@
+</div>
+<hr /><footer role="contentinfo" id="footer">  
+  <div id="inner-footer" class="clearfix container padding-top-bottom">
+  	<?php $options = get_option( 'faster_theme_options' ); ?>
+    <div id="widget-footer" class="clearfix row">
+      <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer1') ) : ?>
+      <?php endif; ?>
+      <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer2') ) : ?>
+      <?php endif; ?>
+      <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer3') ) : ?>
+      <?php endif; ?>
+    </div>
+    <nav class="footer-menu-nav">
+     	<ul class="footer-nav nav navbar-nav">
+        	<?php if($options['fburl'] || $options['twitter'] || $options['googleplus'] || $options['linkedin']) { ?>
+        	<li><a>Follow Us :</a></li>
+            <?php } if($options['fburl']) { ?>
+        	<li class="facebook_icon socia_icon"><a href="<?php echo $options['fburl']; ?>" target="_blank">facebook</a></li>
+            <?php } if($options['twitter']) { ?>
+            <li class="twitter_icon socia_icon"><a href="<?php echo $options['twitter']; ?>">twitter</a></li>
+            <?php } if($options['googleplus']) { ?>
+            <li class="google_icon socia_icon"><a href="<?php echo $options['googleplus']; ?>">google+</a></li>
+            <?php } if($options['linkedin']) { ?>
+            <li class="linkedin_icon socia_icon"><a href="<?php echo $options['linkedin']; ?>">linkedin</a></li>
+            <?php } ?>
+        </ul>
+    </nav>
+    <p class="attribution">
+	<?php 
+		if($options['footertext'] != '')
+		{
+			echo $options['footertext'];
+		}
+		echo "Powered by <a href='http://wordpress.org' target='_blank'>WordPress</a>.";
+		echo " <a href='http://fasterthemes.com/themes/MyWiki' target='_blank'>MyWiki Theme</a> by FasterThemes.";				
+	?>
+    </p>
+  </div>
+  <!-- end #inner-footer --> 
+  
+</footer>
+<!-- end footer -->
+<!-- end #maincont .container --> 
+<?php wp_footer(); // js scripts are inserted using this function ?>
+</body>
+</html>
